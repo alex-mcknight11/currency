2ded59c000cd9c85042007dc19a304f24755d784
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-07-01 09:50:55 -0700

    flesh out functionality

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './currency.js';
+
+function displayExchangeRate(q, rate) {
+  $(".exchangeOutput").text(`The exchange rate from USD to ${q} is ${rate}.`);
+}
