4d99a09ac346c055ef0cca40e9a1f6d96d9ac206
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-07-01 11:16:40 -0700

    detail currency exchange JS code

--- src/index.js
+++ src/index.js
@@ -11,6 +11,39 @@ function clearFields() {
   $(".error").text("");
 }
 
+function getElements(response) {
+  if (response.conversion_rate) {
+    let currentExchangeRate =
+      Math.round((response.conversion_rate + Number.EPSILON) * 100) / 100;
+    let amount = $("#amount").val();
+    $(".display").text(`${currentExchangeRate}`);
+    $(".multiply").text(`${currentExchangeRate * amount}`);
+  } else {
+    $(".display").text("Error");
+    $(".multiply").text("N/A");
+    $(".error").text(
+      `Error`
+    );
+  }
+}
+
 function displayExchangeRate(q, rate) {
   $(".exchangeOutput").text(`The exchange rate from USD to ${secondCurrency} is ${rate}.`);
 }
+
+async function getExchangeRate(USD, secondCurrency) {
+  const response = await ExchangeRate.getCurrency(
+    USD,
+    secondCurrency
+  );
+  getElements(response, USD, secondCurrency);
+}
+
+$(document).ready(function () {
+  $("#currencyInfo").click(function () {
+    let userUSD = $("#USDentry").val();
+    let userCurrency = $("#seconndEntry").val();
+    clearFields();
+    getExchangeRate(userUSD, userCurrency);
+  });
+});
